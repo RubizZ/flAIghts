db35eb9efea1df10d9de69e5101a693a60870f5d
RubizZ <45830465+RubizZ@users.noreply.github.com>
2026-03-02 21:10:12 +0100

    Implement resend email verification code endpoint and fix more stuff regarding a misnamed error

--- frontend/src/pages/EmailVerification.tsx
+++ frontend/src/pages/EmailVerification.tsx
@@ -84,7 +84,7 @@ export default function EmailVerification() {
         verify({
             data: {
                 email: stateEmail,
-                token
+                code: token
             }
         });
     };
