d4ac8d26be172548e23eda35317b9daa239309ef
RubizZ <45830465+RubizZ@users.noreply.github.com>
2026-03-08 17:29:45 +0100

    Add orval to vite lifecycle and typescript checking

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import checker from 'vite-plugin-checker';
+import run from 'vite-plugin-run';
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        tsconfigPaths()
+        tsconfigPaths(),
+        checker({
+            typescript: true
+        }),
+        run({
+            run: ['npm', 'run', 'orval'],
+            condition: (file) => file.includes('openapi.json') || file.includes('orval.config.js'),
+        })
     ],
     server: {
         port: 5173,
