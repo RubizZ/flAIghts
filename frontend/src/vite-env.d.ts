38071e46055fdd6af54c8bd42f3052b6766a8aef
RubizZ <45830465+RubizZ@users.noreply.github.com>
2026-04-25 02:05:05 +0200

    Add env vars to vite types

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GOOGLE_CLIENT_ID: string
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_BACKEND_API_BASE_URL: string;
+  readonly VITE_EVALUATION_MODE: string;
 }
 
 interface ImportMeta {
